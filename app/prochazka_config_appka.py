0e8802b7baf16239e64f342109069304c1bac30c
Jakub Prochazka <jprochazka1@cmgp.local>
2025-01-06 15:37:00 +0100

    Appka

---
+++ app/prochazka_config_appka.py
@@ -0,0 +1,27 @@
+### Program imports
+import datetime as dt
+from datetime import date, datetime, timedelta
+import os
+import sys
+
+### Program configuration
+
+## Working directory configuration
+"""
+This section configures the working directory
+    to ensure the script runs in the current file location.
+"""
+# Get the current working directory (where the script is located)
+current_directory = os.path.dirname(os.path.abspath(__file__))
+# Change the current working directory
+os.chdir(current_directory)
+# Now you can perform operations in the current directory
+print(f"Current working directory is: {os.getcwd()}")
+
+## Backend import to frontend
+
+
+## A Music file configuration
+
+
+## Google Sheets database configuration
